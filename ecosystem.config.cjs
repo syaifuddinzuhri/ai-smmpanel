efca121a7e59d334d4a4cbd235ed6c30d205d11c
Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2026-06-12 16:02:13 +0700

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,18 +5,14 @@ module.exports = {
       script: '.output/server/index.mjs',
       instances: 1,
       exec_mode: 'fork',
+      node_args: '--env-file=.env',
       env: {
         NODE_ENV: 'production',
         PORT: 4731,
-        NUXT_API_BASE_URL: '',
-        NUXT_API_KEY: '',
-        ANTHROPIC_API_KEY: '',
       },
-      // Auto-restart jika crash
       autorestart: true,
       watch: false,
       max_memory_restart: '512M',
-      // Log
       out_file: './logs/out.log',
       error_file: './logs/error.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
